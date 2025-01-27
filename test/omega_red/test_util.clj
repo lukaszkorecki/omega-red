54773dba51706ff0addef004607a02a1febfbf80
Łukasz Korecki <lukasz@korecki.me>
2025-01-27 13:26:17 -0800

    fix env var name

--- test/omega_red/test_util.clj
+++ test/omega_red/test_util.clj
@@ -4,7 +4,7 @@
    [com.stuartsierra.component :as component]))
 
 (def redis-config
-  {:host (or (System/getenv "REDIS_URL") "127.0.0.1")
+  {:host (or (System/getenv "REDIS_HOST") "127.0.0.1")
    :port (Integer/parseInt (or (System/getenv "REDIS_PORT") "6379"))})
 
 (def sys (atom nil))
