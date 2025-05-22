766a05c7105d63bf2e50121827b123e9b006f271
Łukasz Korecki <lukasz@korecki.me>
2025-05-22 15:45:19 -0700

    Fix jar description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.lukaszkorecki/omega-red "2.3.1-SNAPSHOT"
-  :description "Clojure & Component firendly Redis client, baseterd on Jedis"
+  :description "Redis client for Cloure, based on Jedis, with optional Component support"
   :url "https://github.com/nomnom-insights/nomnom.omega-red"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"
