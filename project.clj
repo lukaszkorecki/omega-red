7f5709f2e02b1387f221768230a9e675d368d3c1
Łukasz Korecki <lukasz@korecki.me>
2025-12-05 09:34:28 -0800

    v2.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/omega-red "2.5.1-SNAPSHOT"
+(defproject org.clojars.lukaszkorecki/omega-red "2.6.0-SNAPSHOT"
   :description "Redis client for Cloure, based on Jedis, with optional Component support"
   :url "https://github.com/nomnom-insights/nomnom.omega-red"
   :license {:name "MIT License"
